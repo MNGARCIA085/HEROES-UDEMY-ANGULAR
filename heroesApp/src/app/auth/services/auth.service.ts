1e0ee0f61db6de5de4d41600a4030992a053bcde
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-16 17:39:17 -0500

    guards

--- heroesApp/src/app/auth/services/auth.service.ts
+++ heroesApp/src/app/auth/services/auth.service.ts
@@ -2,18 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _auth: Auth | undefined;
+
+  get auth(): Auth{
+    // {... } es para asegurarme de no cambiarlo
+    return {...this._auth!};
+  }
+
   private baseUrl:string = environment.baseUrl;
 
   constructor(private http:HttpClient) { }
 
   login(){
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+              .pipe(
+                tap(auth => this._auth=auth),
+                tap(auth => localStorage.setItem('id',auth.id))
+              );
+  }
+
+  logout(){
+    this._auth = undefined;
   }
 
 }
