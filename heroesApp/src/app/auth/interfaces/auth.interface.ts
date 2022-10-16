4c6b0751b304a67f1ea49cf028c14f72c4635f86
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-15 21:26:47 -0500

    inicio login

---
+++ heroesApp/src/app/auth/interfaces/auth.interface.ts
@@ -0,0 +1,8 @@
+
+// debe ser como est[a en la base
+
+export interface Auth{
+    id:string,
+    email:string,
+    usuario:string
+}
--- heroesApp/src/app/auth/pages/login/login.component.ts
+++ heroesApp/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+              private authService:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    // TODO: chequear contra el backend
+
+    // consumo el servicio
+    this.authService.login()
+        .subscribe(resp => {
+          console.log(resp); // deber[ian ser los datos del usuario
+          // si tiene id navego a la pantalla principal
+          if (resp.id){
+            this.router.navigate(['./heroes']);
+          }
+        })
+
   }
 
 }
